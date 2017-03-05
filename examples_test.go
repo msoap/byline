660bf41f89c96e857b52212d1d55e8257aad4f74
Sergey Mudrik <sergey.mudrik@gmail.com>
2017-03-05 15:41:19 +0300

    Added examples

--- examples_test.go
+++ examples_test.go
@@ -1,6 +1,7 @@
 package byline_test
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -10,6 +11,58 @@ import (
 
 	"github.com/msoap/byline"
 )
+
+func Example() {
+	reader := strings.NewReader(`CSV Title
+CSV description
+ID,NAME,PRICE
+A001,name one,12.3
+
+A002,second row;7.1
+A003,three row;15.51
+Total: ....
+Some text
+`)
+
+	lr := byline.NewReader(reader).
+		GrepString(func(line string) bool {
+			// skip empty lines
+			return line != "" && line != "\n"
+		}).
+		Grep(func(line []byte) bool {
+			return !bytes.HasPrefix(line, []byte("CSV"))
+		}).
+		SetFS(regexp.MustCompile(`,|;`)).
+		AWKMode(func(line string, fields []string, vars byline.AWKVars) (string, error) {
+			// skip header
+			if strings.HasPrefix(fields[0], "ID") {
+				return "", byline.ErrOmitLine
+			}
+			// skip footer
+			if strings.HasPrefix(fields[0], "Total:") {
+				return "", io.EOF
+			}
+			return line, nil
+		}).
+		MapString(func(line string) string {
+			return "Z" + line
+		}).
+		AWKMode(func(line string, fields []string, vars byline.AWKVars) (string, error) {
+			if vars.NF < 3 {
+				return "", fmt.Errorf("csv parse failed for %q", line)
+			}
+
+			return fmt.Sprintf("%s - %s (line:%d)", fields[0], fields[1], vars.NR), nil
+		})
+
+	result, err := lr.ReadAllString()
+	fmt.Print("\n", result, err)
+	// Output:
+	// ZA001 - name one (line:4)
+	// ZA002 - second row (line:6)
+	// ZA003 - three row (line:7)
+	// <nil>
+}
 
 func ExampleReader_AWKMode() {
 	reader := strings.NewReader(`ID,NAME,PRICE
@@ -135,5 +188,35 @@ Some text
 	// A001,name one,12.3
 	// A002,second row;7.1
 	// A003,three row;15.51
+	// <nil>
+}
+
+func ExampleReader_MapStringErr() {
+	reader := strings.NewReader(`
+100000
+200000
+300000
+end ...
+Some text
+`)
+
+	result, err := byline.NewReader(reader).
+		MapStringErr(func(line string) (string, error) {
+			switch {
+			case line == "" || line == "\n":
+				return "", byline.ErrOmitLine
+			case strings.HasPrefix(line, "end "):
+				return "", io.EOF
+			default:
+				return "<" + line, nil
+			}
+		}).
+		ReadAllString()
+
+	fmt.Print("\n"+result, err)
+	// Output:
+	// <100000
+	// <200000
+	// <300000
 	// <nil>
 }
