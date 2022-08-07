5b43e524e5b589975ba3867754ca40d2c3270c8e
Serhii Mudryk <sergey.mudrik@gmail.com>
2022-08-07 17:44:03 +0300

    gofmt with Go 1.19

--- doc.go
+++ doc.go
@@ -4,26 +4,25 @@ You can add UNIX text processing principles to its Reader (like with awk, grep, 
 
 Install
 
- go get -u github.com/msoap/byline
+	go get -u github.com/msoap/byline
 
 Usage
 
- import "github.com/msoap/byline"
+	import "github.com/msoap/byline"
 
- // Create new line-by-line Reader from io.Reader:
- lr := byline.NewReader(reader)
+	// Create new line-by-line Reader from io.Reader:
+	lr := byline.NewReader(reader)
 
- // Add to the Reader stack of a filter functions:
- lr.MapString(func(line string) string {return "prefix_" + line}).GrepByRegexp(regexp.MustCompile("only this"))
+	// Add to the Reader stack of a filter functions:
+	lr.MapString(func(line string) string {return "prefix_" + line}).GrepByRegexp(regexp.MustCompile("only this"))
 
- // Read all content
- result, err := lr.ReadAll()
+	// Read all content
+	result, err := lr.ReadAll()
 
- // Use everywhere instead of io.Reader
- _, err := io.Copy(os.Stdout, lr)
+	// Use everywhere instead of io.Reader
+	_, err := io.Copy(os.Stdout, lr)
 
- // Or in one place
- result, err := byline.NewReader(reader).MapString(func(line string) string {return "prefix_" + line}).ReadAll()
-
+	// Or in one place
+	result, err := byline.NewReader(reader).MapString(func(line string) string {return "prefix_" + line}).ReadAll()
 */
 package byline
